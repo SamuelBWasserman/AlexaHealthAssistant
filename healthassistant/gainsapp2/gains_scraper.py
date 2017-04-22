5f56ea02b395a0ec4654f6bd97b78f3a2643d556
SamuelBWasserman <sambwasserman@gmail.com>
2017-04-22 17:19:21 -0400

    yolo scraper

---
+++ healthassistant/gainsapp2/gains_scraper.py
@@ -0,0 +1,23 @@
+from bs4 import BeautifulSoup
+import urllib2
+
+def scrape():
+    current_site = 'https://www.bodybuilding.com/category/training'
+    try:
+        url = urllib2.urlopen(current_site).read()
+    except:
+        return
+    article_dict = dict()
+    article_info = []
+    soup = BeautifulSoup(url, 'html.parser')
+    article_titles = soup.findAll('span',{'class':'title'})
+    for tag in article_titles:
+        article_info.append(tag.string)
+        link = tag.find('a')
+        try:
+            article_info.append(link['href'])
+        except:
+            pass
+        article_dict.update({article_info[1]:article_info[0]})
+        article_info = []
+    yield article_dict
