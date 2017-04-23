72cbea93d77d467fe5c37ab320163f02ed0f8d0d
SamuelBWasserman <sambwasserman@gmail.com>
2017-04-22 20:03:29 -0400

    Lots of changes

--- healthassistant/gainsapp2/gains_scraper.py
+++ healthassistant/gainsapp2/gains_scraper.py
@@ -1,23 +1,35 @@
 from bs4 import BeautifulSoup
 import urllib2
+import scrapy
+from pymongo import MongoClient
 
-def scrape():
-    current_site = 'https://www.bodybuilding.com/category/training'
-    try:
-        url = urllib2.urlopen(current_site).read()
-    except:
-        return
-    article_dict = dict()
-    article_info = []
-    soup = BeautifulSoup(url, 'html.parser')
-    article_titles = soup.findAll('span',{'class':'title'})
-    for tag in article_titles:
-        article_info.append(tag.string)
-        link = tag.find('a')
+class GainsSpider(scrapy.Spider):
+    name = 'gainsspider'
+    start_urls = ['https://www.bodybuilding.com/category/training']
+
+    def parse(self, response):
+        client = MongoClient()
+        db = client.test
+        current_site = 'https://www.bodybuilding.com/category/training'
         try:
-            article_info.append(link['href'])
+            url = urllib2.urlopen(current_site).read()
         except:
-            pass
-        article_dict.update({article_info[1]:article_info[0]})
+            return
+        article_dict = dict()
         article_info = []
-    yield article_dict
+        soup = BeautifulSoup(url, 'html.parser')
+        article_titles = soup.findAll('span',{'class':'title'})
+        count = 0
+        for tag in article_titles:
+            article_info.append(tag.string)
+            link = tag.find('a')
+            try:
+                article_info.append(link['href'])
+                article_dict.update({ '{}{}'.format('title', count):article_info[0]})
+                article_dict.update({ '{}{}'.format('urls', count):article_info[1]})
+                count = count + 1
+                article_info = []
+            except:
+                pass
+        db.insert_one(article_dict)
+        yield article_dict
